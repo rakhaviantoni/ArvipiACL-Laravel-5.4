86933ea1ff12508b76638ba8b2fe85e16e9ed23b
rakhaviantoni <rakha.rvp@gmail.com>
2017-04-18 05:22:00 +0700

    Add files

---
+++ storage/framework/views/0716a89c2919777377095e37a5bc2dec1a5e6f5e.php
@@ -0,0 +1,117 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+          <ul class="breadcrumb">
+          <li><a href="<?php echo e(url('/home')); ?>">Dashboard</a></li>
+          <li class="active">Press Release</li>
+          </ul>
+
+          <div id="create" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                  <h4 class="modal-title">Create Press Release</h4>
+                </div>
+                <form class="form-horizontal" role="form" method="POST" action="<?php echo e(url('/home/press/store')); ?>">
+                    <?php echo e(csrf_field()); ?>
+
+                <div class="modal-body">
+                  <div class="form-group<?php echo e($errors->has('title') ? ' has-error' : ''); ?>">
+                      <label for="name" class="col-md-4 control-label">Title</label>
+
+                      <div class="col-md-6">
+                          <input id="title" type="text" class="form-control" name="title" value="<?php echo e(old('title')); ?>" required autofocus>
+
+                          <?php if($errors->has('title')): ?>
+                              <span class="help-block">
+                                  <strong><?php echo e($errors->first('title')); ?></strong>
+                              </span>
+                          <?php endif; ?>
+                      </div>
+                  </div>
+                  <div class="form-group<?php echo e($errors->has('category') ? ' has-error' : ''); ?>">
+                      <label for="category" class="col-md-4 control-label">Category</label>
+
+                      <div class="col-md-6">
+                          <input id="category" type="text" class="form-control" name="category" value="<?php echo e(old('category')); ?>" required autofocus>
+
+                          <?php if($errors->has('category')): ?>
+                              <span class="help-block">
+                                  <strong><?php echo e($errors->first('category')); ?></strong>
+                              </span>
+                          <?php endif; ?>
+                      </div>
+                  </div>
+                  <div class="form-group<?php echo e($errors->has('description') ? ' has-error' : ''); ?>">
+                      <label for="description" class="col-md-4 control-label">Description</label>
+
+                      <div class="col-md-6">
+                          <textarea id="description" type="text" class="form-control" name="description" value="<?php echo e(old('description')); ?>" required autofocus></textarea>
+
+                          <?php if($errors->has('description')): ?>
+                              <span class="help-block">
+                                  <strong><?php echo e($errors->first('description')); ?></strong>
+                              </span>
+                          <?php endif; ?>
+                      </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <input id="userid" type="hidden" class="form-control" name="userid" value="<?php echo e(Auth::user()->userid); ?>">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Create</button>
+                </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Press Release</div>
+
+                <div class="panel-body">
+                  <button type="button" data-toggle="modal" data-target="#create" class="btn btn-primary" title="Create New"><span class="glyphicon glyphicon-plus"></span></button><br><br>
+                  <table id="press-table" class="table-stripped">
+                      <thead>
+                      <tr>
+                          <th>Title</th>
+                          <th>Slug</th>
+                          <th>Time</th>
+                          <th>Category</th>
+                          <th>Description</th>
+                          <th>User</th>
+                          <th>Action</th>
+                      </tr>
+                      </thead>
+                  </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('scripts'); ?>
+<script>
+    $(function () {
+        $('#press-table').DataTable({
+            serverSide: true,
+            processing: true,
+            ajax: '/home/press/data',
+            columns: [
+                {data: 'title'},
+                {data: 'slug'},
+                {data: 'created_at'},
+                {data: 'category'},
+                {data: 'description'},
+                {data: 'user.username'},
+                {data: 'action', orderable: false, searchable: false}
+            ]
+        });
+    });
+</script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/8e3593c51d6cec3e66274695020474cf3d76a2d4.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(config('app.locale')); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Arvipi Access Control</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="<?php echo e(asset('css/bootstrap.min.css')); ?>" rel="stylesheet" type="text/css">
+        <script src="<?php echo e(asset('js/jquery-3.2.0.min.js')); ?>" charset="utf-8"></script>
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <?php if(Route::has('login')): ?>
+                <div class="top-right links">
+                    <?php if(Auth::check()): ?>
+                        <a href="<?php echo e(url('/home')); ?>">Home</a>
+                    <?php else: ?>
+                        <a href="<?php echo e(url('/login')); ?>">Login</a>
+                        <a href="<?php echo e(url('/register')); ?>">Register</a>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Arvipi Access Control
+                </div>
+
+                <div class="links">
+                    <a href="#about" data-toggle="tab" aria-expanded="true">About</a>
+                    <a href="#press" data-toggle="tab" aria-expanded="true">Press</a>
+                    <a href="https://github.com/rakhaviantoni/ArvipiACL-Laravel-5.4" target="_blank">GitHub</a>
+                    <a href="#contact" data-toggle="tab" aria-expanded="true">Contact</a>
+                  </div><br>
+                    <div id="myTabContent" class="col-md-12 tab-content">
+                      <div class="tab-pane fade active in" id="about">
+                        <p>PT. Arvipi Limited is an IT company that's formed in 2017 where their first application (Adoreo) launched with more than 100,000 registered users in a month. With their focus on creating solution for the country, they are able to grow up fast with providing some Web Service that able to helps other’s production efficiency.</p>
+                      </div>
+                      <div class="tab-pane fade" id="press">
+                        <?php $__currentLoopData = $press; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $press): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <div class="panel panel-primary">
+                          <div class="panel-heading">
+                            <h3 class="panel-title"><?php echo e($press->title); ?> on <?php echo e(Carbon\Carbon::parse($press->created_at)->toFormattedDateString()); ?> by <?php echo e($press->user->username); ?></h3>
+                          </div>
+                          <div class="panel-body" style="text-align:left">
+                            <?php echo $press->description; ?>
+
+                          </div>
+                        </div>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                      </div>
+                      <div class="tab-pane fade" id="contact">
+                        <p>rakhaviantoni.my.id</p>
+                      </div>
+                    </div>
+            </div>
+        </div>
+        <script src="<?php echo e(asset('js/bootstrap.min.js')); ?>"></script>
+    </body>
+</html>
